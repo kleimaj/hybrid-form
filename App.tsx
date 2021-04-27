db1742433a093500e75f0d96846a83523f670329
jacob kleiman <jacobakleiman@gmail.com>
2021-04-27 13:50:59 -0700

    adds touchablewithoutfeedback for forms

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Input from './components/Input';
 
 export default function App() {
+  const [res, setRes] = useState({});
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
