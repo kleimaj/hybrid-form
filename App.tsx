60a1deb6ce0a244d4b64e62fcec32c3f38918c0e
jacob kleiman <jacobakleiman@gmail.com>
2021-04-27 16:18:40 -0700

    implements one Input in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { TouchableWithoutFeedback, Keyboard } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import Input from './components/Input';
 
 export default function App() {
@@ -12,6 +17,12 @@ export default function App() {
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.container}>
         <Text>Hybrid</Text>
+        <Input
+          label={'New Address'}
+          value={res['New Address']}
+          onChangeValue={(value) => console.log(value)}
+          keyboardType={'default'}
+        />
       </View>
     </TouchableWithoutFeedback>
   );
