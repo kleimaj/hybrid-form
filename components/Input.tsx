69b896b6260ddcdf400891d2eda8363a1570837e
jacob kleiman <jacobakleiman@gmail.com>
2021-04-27 13:36:07 -0700

    passes usestate as props

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+type KeyboardTypes =
+  | 'default'
+  | 'number-pad'
+  | 'decimal-pad'
+  | 'numeric'
+  | 'email-address'
+  | 'phone-pad';
 
 type InputProps = {
   label: string;
+  keyboardType?: KeyboardTypes;
+  value: string;
+  onChangeValue?: Dispatch<SetStateAction<string>>;
 };
 
-export default function Input({ label }: InputProps) {
+export default function Input({
+  label,
+  keyboardType,
+  value,
+  onChangeValue,
+}: InputProps) {
   return (
     <View style={styles.container}>
       <Text>{label}</Text>
+      <TextInput
+        onChangeText={onChangeValue}
+        value={value}
+        keyboardType={keyboardType}
+      />
     </View>
   );
 }
